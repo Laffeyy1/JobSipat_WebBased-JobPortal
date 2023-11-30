3934df8429cbeba1e96723158f8253161fb96c2c
hoppylng <dexteranimaljam@gmail.com>
2023-11-30 20:41:06 +0800

    move files, direction chuchu

--- JobSipat/main/views.py
+++ JobSipat/main/views.py
@@ -6,5 +6,17 @@ from django.http import HttpResponse
 def index(response):
     return render(response, "main/index.html",{})
 
-def home(response):
-    return render(response, "main/index.html",{})
+def signup(response):
+    return render(response, "main/Signup.html",{})
+
+def employeeProfile(response):
+    return render(response, "main/employee-profile.html",{})
+
+def employeeNotif(response):
+    return render(response, "main/employee-notif.html",{})
+
+def employeeJobs(response):
+    return render(response, "main/employee-jobs.html",{})
+
+def employeeEdit(response):
+    return render(response, "main/employee-edit.html",{})
