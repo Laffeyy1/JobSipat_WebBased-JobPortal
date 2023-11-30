7440ac98569e98b7d9982107139ca4f555efdea9
Laffey <plutonicp@gmail.com>
2023-11-30 18:08:08 +0800

    minor updates
    -templates
    -static files (css,js, img)

--- JobSipat/main/views.py
+++ JobSipat/main/views.py
@@ -4,4 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(response):
-    return HttpResponse("<h1>Hello World!</h1>")
+    return render(response, "main/index.html",{})
+
+def home(response):
+    return render(response, "main/index.html",{})
